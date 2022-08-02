0fac31d022a30ee391b2c8395ac4a1ba9946dd5d
lingmx <529247970@qq.com>
2022-08-02 23:45:50 +0800

    Update App.vue

--- vue3/day1/vue-begin01/src/App.vue
+++ vue3/day1/vue-begin01/src/App.vue
@@ -5,7 +5,9 @@ export default {
     return {
       num: 0,
       uname: '张三',
-      msg: "<h2>标题</h2>"
+      msg: "<h2>标题</h2>",
+      id: 'd1',
+      url: 'https://t7.baidu.com/it/u=234231168,3860178184&fm=167&app=3000&size=w560&n=0&f=JPEG&fmt=auto?sec=1659546000&t=9b92904899b0f809a4f9c349ee8e4189'
     }
   },
   methods: {
@@ -14,6 +16,9 @@ export default {
       // this 指向vue实例
       this.uname = "李四"
     },
+    changeColor: function () {
+      this.id = "d2"
+    }
   }
 }
 </script>
@@ -27,9 +32,28 @@ export default {
     <!-- v-html 让内容以html的形式显示 -->
     <p v-html="msg"></p>
     <button @click="changeUname">改变名字</button>
+    <!-- v-bind:绑定属性的内容 -->
+    <p v-bind:id="id">v-bind绑定</p>
+    <p><img v-bind:src="url" alt=""/></p>
+    <button @click="changeColor">改变颜色</button>
+    <!-- 使用JavaScript表达式 -->
+    <button @click="id = 'd1'">改变颜色(JavaScript)</button>
+    <p>{{ num + 1 }}</p>
+    <p>{{ uname.split("").reverse().join("") }}</p>
+    <p v-bind:id="id + 1">v-bind绑定</p>
+    <!-- 语法糖 ：-->
+    <!-- v-on:用于监听 DOM 事件 -->
+    <button v-on:click="changeColor">改变颜色</button>
+    <!-- v-on语法糖： @ -->
   </div>
 </template>
 
 <style scoped>
+#d1 {
+  color: #faa;
+}
 
+#d2 {
+  color: #afa;
+}
 </style>
