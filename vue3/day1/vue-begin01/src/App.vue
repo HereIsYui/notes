e76267d2487b86d65ed27482eab0aba8ae34456e
lingmx <529247970@qq.com>
2022-08-02 22:44:07 +0800

    Update App.vue

--- vue3/day1/vue-begin01/src/App.vue
+++ vue3/day1/vue-begin01/src/App.vue
@@ -1,32 +1,35 @@
 <script>
 // 声明式渲染，可以提高开发效率
-export default{
-  data(){
-    return{
-      num:0,
-      uname:'张三'
+export default {
+  data() {
+    return {
+      num: 0,
+      uname: '张三',
+      msg: "<h2>标题</h2>"
     }
+  },
+  methods: {
+    // 给vue定义方法
+    changeUname: function () {
+      // this 指向vue实例
+      this.uname = "李四"
+    },
   }
 }
 </script>
 
 <template>
   <div>
-    <p>{{num}}</p>
-    <p>{{uname}}</p>
+    <p>{{ num }}</p>
+    <p>{{ uname }}</p>
+    <!-- v-once当数据改变是，插值处的内容不会更新 -->
+    <p v-once>{{ uname }}</p>
+    <!-- v-html 让内容以html的形式显示 -->
+    <p v-html="msg"></p>
+    <button @click="changeUname">改变名字</button>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
